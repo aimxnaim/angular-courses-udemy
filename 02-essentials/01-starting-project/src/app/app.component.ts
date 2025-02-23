e8576fa2a59b6bcc63ba0adbcc811318c26268a5
aimxnaim <aimxnaim@gmail.com>
2025-02-23 22:28:16 +0800

    refactor: Update user component to display multiple users and emit selection event

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectedUser(id: string) {
+    console.log('Selected user with id: ', id);
+  }
+}
--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-// import { DUMMY_USERS } from '../dummy-users';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,19 +7,17 @@ import { DUMMY_USERS } from './dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  randomIndex : number = Math.floor(Math.random() * DUMMY_USERS.length)
-  selectedUser : any = signal(DUMMY_USERS[this.randomIndex]);
+  @Input({required: true}) id!: string;
+  @Input() avatar!: string;
+  @Input() name!: string;
+  @Output() select = new EventEmitter;
 
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+  get imagePath(){ 
+    return `assets/users/${this.avatar}`;
+   }
 
-  // ? This is the same as the above code, but below is act like a property
-  // get imagePath(){
-  //   return `assets/users/${this.selectedUser().avatar}`;
-  // }
-
-  onSelectedUser(){
-    this.randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
-  }
-
+   onSelectedUser(){
+    this.select.emit(this.id);
+   }
+   
 }
