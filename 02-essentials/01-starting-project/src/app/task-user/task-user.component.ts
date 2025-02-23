6564a7193c759ebdc34a8bbbca932b5979c0f7c1
aimxnaim <aimxnaim@gmail.com>
2025-02-23 22:31:59 +0800

    feat: Add task user component with initial template and styles

---
+++ 02-essentials/01-starting-project/src/app/task-user/task-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-user.component.html',
+  styleUrl: './task-user.component.css'
+})
+export class TaskUserComponent {
+
+}
