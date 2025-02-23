93d6f39aea1ef12ebdc9185d8a8195dd3e8d98a2
aimxnaim <aimxnaim@gmail.com>
2025-02-23 23:47:36 +0800

    feat: Implement dynamic user selection and display tasks for selected user

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-users';
 import { CommonModule } from '@angular/common';
+import { TaskUserComponent } from './task-user/task-user.component';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,21 @@ import { CommonModule } from '@angular/common';
   imports: [
     HeaderComponent,
     UserComponent,
-    CommonModule
+    CommonModule,
+    TaskUserComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  id!: string ;
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.id);
+  }
 
   onSelectedUser(id: string) {
-    console.log('Selected user with id: ', id);
+    this.id = id;
   }
 }
--- 02-essentials/01-starting-project/src/app/task-user/task-user.component.ts
+++ 02-essentials/01-starting-project/src/app/task-user/task-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-task-user',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './task-user.component.css'
 })
 export class TaskUserComponent {
-
+  id = input.required<string | undefined>()
 }
