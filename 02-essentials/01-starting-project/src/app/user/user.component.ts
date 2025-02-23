f23192ae5d37029306583308a3aa6076d8fe2e07
aimxnaim <aimxnaim@gmail.com>
2025-02-23 20:26:00 +0800

    Add dummy user data and update user component for dynamic user selection

--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DUMMY_USERS } from './dummy-users';
+// import { DUMMY_USERS } from '../dummy-users';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  randomIndex : number = Math.floor(Math.random() * DUMMY_USERS.length)
+  selectedUser : any = DUMMY_USERS[this.randomIndex];
+
+  get imagePath(){
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onSelectedUser(){
+    this.randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[this.randomIndex];
+  }
 
 }
