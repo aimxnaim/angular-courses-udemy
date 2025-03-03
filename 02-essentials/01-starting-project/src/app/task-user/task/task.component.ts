138c99653e936219a723261d120abb998141d8e2
aimxnaim <aimxnaim@gmail.com>
2025-03-03 14:44:10 +0800

    feat: Format task due date and refactor task completion handling with service injection

--- 02-essentials/01-starting-project/src/app/task-user/task/task.component.ts
+++ 02-essentials/01-starting-project/src/app/task-user/task/task.component.ts
@@ -1,19 +1,21 @@
-import { Component, EventEmitter, input, Output, output } from '@angular/core';
+import { Component, inject, input} from '@angular/core';
 import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TaskUserService } from '../task-user.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   task = input.required<any>()
-  @Output() complete = new EventEmitter<string>()
+  private taskService = inject(TaskUserService)
 
   onSelectedTask() {
-    this.complete.emit(this.task().id)
+    this.taskService.completeTask(this.task().id);
   }
 
 }
