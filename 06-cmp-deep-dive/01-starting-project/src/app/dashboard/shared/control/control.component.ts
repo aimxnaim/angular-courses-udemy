9be192f0e6025bfc3489682c6a2589657492f9cf
aimxnaim <aimxnaim@gmail.com>
2025-03-23 15:58:59 +0800

    feat: add click event handler to Control component for improved interactivity

--- 06-cmp-deep-dive/01-starting-project/src/app/dashboard/shared/control/control.component.ts
+++ 06-cmp-deep-dive/01-starting-project/src/app/dashboard/shared/control/control.component.ts
@@ -8,11 +8,17 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
   label = input.required<string>();
   for = input.required<string>();
+
+  onClick() {
+    console.log('Control clicked!');
+  }
+
 
 }
