de3a59ddc3c591bc717a61da1cf2b9f5aa81d2c7
aimxnaim <aimxnaim@gmail.com>
2025-04-08 11:30:18 +0800

    feat: update ticket model and component to use 'text' instead of 'request', add ticket rendering in tickets component
    Renames request to text and displays tickets
    
    Improves property naming for clarity
    Optimizes UI rendering with trackBy for better performance

--- 06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/ticket.model.ts
+++ 06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/ticket.model.ts
@@ -1,7 +1,7 @@
 export interface Ticket {
     id: string;
     title: string;
-    request: string;
+    text: string;
     status: 'open' | 'closed';
     date: Date;
 }
--- 06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -2,11 +2,13 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from './ticket.model';
+import { CommonModule } from '@angular/common';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [DashboardItemComponent, NewTicketComponent],
+  imports: [DashboardItemComponent, NewTicketComponent, CommonModule, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
   encapsulation: ViewEncapsulation.Emulated
@@ -18,11 +20,15 @@ export class TicketsComponent {
     const newTicket: Ticket = {
       id: Math.random().toString(36).substring(2, 9),
       title: ticketInfo.title,
-      request: ticketInfo.text,
+      text: ticketInfo.text,
       status: 'open',
       date: new Date()
     };
     this.tickets.push(newTicket);
     console.log('New ticket added:', this.tickets);
+  }
+
+  trackById(){
+    return (index: number, ticket: Ticket) => ticket.id;
   }
 }
