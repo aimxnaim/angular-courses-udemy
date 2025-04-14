ee91fe5ce23e125be531a1b3a6f594f46fe76581
aimxnaim <aimxnaim@gmail.com>
2025-04-14 09:18:32 +0800

    feat: update user component to navigate to user tasks with routerLink

--- 14-routing/01-starting-project/src/app/app.routes.ts
+++ 14-routing/01-starting-project/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'tasks',
         component: TaskComponent
+    },
+    {
+        path: 'users/:userId',
+        component: UserTasksComponent
     }
 ]
