fe980296fb7f867a01454b12f807696b55b891fa
aimxnaim <aimxnaim@gmail.com>
2025-04-28 12:48:32 +0800

    refactor: update routing configuration for user tasks with improved resolver handling

--- 14-routing/01-starting-project/src/app/app.routes.ts
+++ 14-routing/01-starting-project/src/app/app.routes.ts
@@ -1,9 +1,23 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent, resolveTitle, resolveUserName } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes } from "./users/users.routes";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segment) => {
+    // ? This is a dummy function to show how to use canMatch
+    // ? This will be used to check if the route can be activated or not
+    // ? This will be used to check if the user is logged in or not
+    // ? This will be used to check if the user has permission to access the route or not
+    const router = inject(Router);
+    const shouldgetAccess = Math.random()
+    if(shouldgetAccess > 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
     {
@@ -26,7 +40,8 @@ export const routes: Routes = [
         resolve: {
             userName: resolveUserName // ? This is a resolver function, it will be called before the route is activated
         },
-        title: resolveTitle // ? This is a resolver function, it will be called before the route is activated
+        title: resolveTitle, // ? This is a resolver function, it will be called before the route is activated,
+        canMatch: [dummyCanMatch] // ? This is a canMatch function, it will be called before the route is activated
     },
     {
         path: '**',
