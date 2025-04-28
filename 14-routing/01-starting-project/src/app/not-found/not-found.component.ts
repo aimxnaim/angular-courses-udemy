49db1b370679c8308d5bc29196d2f1a4af44c2cd
aimxnaim <aimxnaim@gmail.com>
2025-04-28 14:41:33 +0800

    fix: correct component decorator formatting in NotFoundComponent

--- 14-routing/01-starting-project/src/app/not-found/not-found.component.ts
+++ 14-routing/01-starting-project/src/app/not-found/not-found.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
+})
+
 export class NotFoundComponent {
 
 }
