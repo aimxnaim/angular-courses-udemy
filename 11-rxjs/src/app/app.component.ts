6927bc64c30947b16d4cc870fc44a917d009a4c7
aimxnaim <aimxnaim@gmail.com>
2025-05-12 10:56:52 +0800

    feat: implement RxJS interval subscription with proper cleanup

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
 
+  private destroyRef = inject(DestroyRef)
+  subscription: any
+
+  ngOnInit(): void {
+    const time$ = interval(1000).pipe(
+      take(5)
+    )
+    time$
+    this.subscription = time$.subscribe({
+      next: (v) => console.log(v),
+      complete: () => console.log('complete')
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroyRef.onDestroy(() => {
+      this.subscription.unsubscribe()
+    })  
+  }
 }
