6d5c976a0b5bc4cf71c752e11fdfbfdf9d1abac6
aimxnaim <aimxnaim@gmail.com>
2025-05-12 11:04:01 +0800

    feat: enhance interval subscription to include mapping for doubled values

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,30 +1,31 @@
 import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
-import { interval, take } from 'rxjs';
+import { interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, OnDestroy {
 
   private destroyRef = inject(DestroyRef)
   subscription: any
 
   ngOnInit(): void {
-    const time$ = interval(1000).pipe(
-      take(5)
-    )
-    time$
-    this.subscription = time$.subscribe({
-      next: (v) => console.log(v),
-      complete: () => console.log('complete')
-    })
+    this.subscription = interval(1000)
+      .pipe(
+        take(5),
+        map((v) => v * 2)
+      )
+      .subscribe({
+        next: (v) => console.log(v),
+        complete: () => console.log('complete')
+      })
   }
 
   ngOnDestroy(): void {
     this.destroyRef.onDestroy(() => {
       this.subscription.unsubscribe()
-    })  
+    })
   }
 }
