db6f3cb3b5808a9bb5adb46e2942cc5f7f54a2d2
aimxnaim <aimxnaim@gmail.com>
2025-05-12 11:29:19 +0800

    feat: integrate toObservable for onCountClick signal and update subscription logic

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, OnDestroy, OnInit, effect, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map, take } from 'rxjs';
 
 @Component({
@@ -11,14 +12,19 @@ export class AppComponent implements OnInit, OnDestroy {
   private destroyRef = inject(DestroyRef)
   subscription: any
   onCountClick = signal(0)
+  onCountClick$ = toObservable(this.onCountClick)
 
   constructor() {
-    effect(() => {
-      console.log('onCountClick', this.onCountClick())
-    })
+    // effect(() => {
+    //   console.log('onCountClick', this.onCountClick())
+    // })
   }
 
   ngOnInit(): void {
+    this.subscription = this.onCountClick$.subscribe({
+      next: (v) => console.log('onCountClick$', v),
+      complete: () => console.log('complete')
+    })
     // this.subscription = interval(1000)
     //   .pipe(
     //     take(5),
