bdd0fcaea5a0ed62c365d7189d8fd2d2dc242067
aimxnaim <aimxnaim@gmail.com>
2025-05-18 09:59:50 +0800

    Refactor login component: simplify template and remove unused form handling logic

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,67 +1,9 @@
-import { Component, DestroyRef, ViewChild, afterNextRender, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { Subscription, debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  @ViewChild('form') form!: NgForm;
-  private destroyRef = inject(DestroyRef);
-  private subscription!: Subscription;
-
-  // ? Below is the oldest way to subscribe to form changes
-  // constructor() {
-  //   afterNextRender(() => {
-  //     this.form().valueChanges?.subscribe(({
-  //       next: (value) => console.log('Form Value:', value),
-  //     }))
-  //   })
-  // }
-
-  ngAfterViewInit() {
-
-    const savedForm = window.localStorage.getItem('formValue');
-
-    if (savedForm) {
-      const loadedForm = JSON.parse(savedForm);
-      const savedEmail = loadedForm.email;
-      setTimeout(() => {
-        this.form.setValue({
-          email: savedEmail,
-          password: ''
-        });
-      }, 100)
-    }
-    // ? After view initialization, subscribe to form changes
-    if (this.form.valueChanges) {
-      this.subscription = this.form.valueChanges?.pipe(debounceTime(500)).subscribe((value) => {
-        console.log('Form Value:', value);
-        window.localStorage.setItem('formValue', JSON.stringify({ email: value.email }))
-      });
-    }
-
-    // ? Below is to unsubscribe from the subscription
-    this.destroyRef.onDestroy(() => this.subscription?.unsubscribe())
-
-
-  }
-  onSubmit(formData: NgForm) {
-
-    if (formData.invalid) {
-      return;
-    }
-    const email = formData.form.value.email;
-    const password = formData.form.value.password;
-
-    // console.log('Email:', email);
-    // console.log('Password:', password);
-    // console.log('FormData:', formData);
-  }
-}
+export class LoginComponent {}
