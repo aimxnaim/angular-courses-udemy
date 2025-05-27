7e3d289e70806d8d566e1e32627a1d99cfc7e458
aimxnaim <aimxnaim@gmail.com>
2025-05-27 09:54:04 +0800

    feat: add home and login components with reactive and template-driven forms
    - Implemented HomeComponent to display courses categorized as Beginners and Advanced.
    - Created LoginReactiveComponent for reactive form-based login.
    - Developed LoginComponent for template-driven login.
    - Added necessary CSS styles for both components.
    - Introduced models for Course and Lesson.
    - Implemented services for fetching courses and lessons data.
    - Added custom validators for password strength and course title uniqueness.
    - Created a pipe to handle error messages in forms.
    - Set up environment configurations and basic application structure.

---
+++ deep-dive-forms-advance/src/app/pipes/only-one-error.pipe.ts
@@ -0,0 +1,28 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+
+@Pipe({
+    name: 'onlyOneError',
+    standalone: false
+})
+export class OnlyOneErrorPipe implements PipeTransform {
+
+    transform(allErrors: any, errorsPriority:string[]): any {
+
+        if (!allErrors) {
+            return null;
+        }
+
+        const onlyOneError: any = {};
+
+        for (let error of errorsPriority) {
+            if (allErrors[error]) {
+                onlyOneError[error] = allErrors[error];
+                break;
+            }
+        }
+
+        return onlyOneError;
+    }
+
+}
